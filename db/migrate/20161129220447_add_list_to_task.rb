ae310be008042a0020f01d86313930e9d8cf32a4
Andre Macedo <andrefrmacedo@gmail.com>
2016-11-29 22:13:00 +0000

    Add list model&relationship

---
+++ db/migrate/20161129220447_add_list_to_task.rb
@@ -0,0 +1,5 @@
+class AddListToTask < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tasks, :list_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129160523) do
+ActiveRecord::Schema.define(version: 20161129220447) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.boolean  "done",       default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "list_id"
   end
 
 end
