975fbbaa89eb3ffd99395680106a8765171fe5f6
Andre Macedo <andrefrmacedo@gmail.com>
2016-11-30 00:20:01 +0000

    Add create list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,21 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def new
+    @new_list = List.new
+  end
+
+  def create
+    @new_list = List.create(list_params)
+
+    redirect_to lists_path
+  end
+
+
+  private 
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
